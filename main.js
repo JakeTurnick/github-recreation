9031b670d7d7bf1829bed517f681b119406292f6
JakeTurnick <jaketurnick@gmail.com>
2023-02-03 12:43:55 -0500

    language color bubble correctly colors

--- main.js
+++ main.js
@@ -4,52 +4,54 @@ const BASE_URL = "https://api.github.com/users/JakeTurnick";
 const ORG_URL = "https://api.github.com/users/JakeTurnick/orgs";
 const REPO_URL = "https://api.github.com/users/JakeTurnick/repos";
 const userName = "JakeTurnick";
+const langBubble = document.querySelectorAll(".color-tag");
+console.log("langBubble", langBubble);
 
 // PROFILE SECTION
 function generateProfile(data) {
-  const source = document.getElementById("profile-template").innerHTML;
-  const template = Handlebars.compile(source);
+	const source = document.getElementById("profile-template").innerHTML;
+	const template = Handlebars.compile(source);
 
-  const html = template(data);
+	const html = template(data);
 
-  document
-    .querySelector(".profile-container")
-    .insertAdjacentHTML("afterbegin", html);
+	document
+		.querySelector(".profile-container")
+		.insertAdjacentHTML("afterbegin", html);
 }
 
 // ORGANIZATION SECTION
 function generateOrgs(data) {
-  const newData = { orgs: data };
-  const source = document.getElementById("org-template").innerHTML;
-  const template = Handlebars.compile(source);
+	const newData = { orgs: data };
+	const source = document.getElementById("org-template").innerHTML;
+	const template = Handlebars.compile(source);
 
-  const html = template(newData);
+	const html = template(newData);
 
-  document.querySelector(".org-container").insertAdjacentHTML("afterend", html);
+	document.querySelector(".org-container").insertAdjacentHTML("afterend", html);
 }
 
 // REPO SECTION
 function generateRepos(data) {
-  const newData = { repos: data };
-  console.log("newdata", newData);
-  const source = document.getElementById("repo-template").innerHTML;
-  const template = Handlebars.compile(source);
+	const newData = { repos: data };
+	console.log("newdata", newData);
+	const source = document.getElementById("repo-template").innerHTML;
+	const template = Handlebars.compile(source);
 
-  const html = template(newData);
+	const html = template(newData);
 
-  document
-    .querySelector(".repo-container")
-    .insertAdjacentHTML("afterbegin", html);
+	document
+		.querySelector(".repo-container")
+		.insertAdjacentHTML("afterbegin", html);
 }
 
 fetch(BASE_URL)
-  .then((response) => response.json())
-  .then((data) => generateProfile(data));
+	.then((response) => response.json())
+	.then((data) => generateProfile(data));
 
 fetch(ORG_URL)
-  .then((response) => response.json())
-  .then((data) => generateOrgs(data));
+	.then((response) => response.json())
+	.then((data) => generateOrgs(data));
 
 fetch(REPO_URL)
-  .then((response) => response.json())
-  .then((data) => generateRepos(data));
+	.then((response) => response.json())
+	.then((data) => generateRepos(data));
